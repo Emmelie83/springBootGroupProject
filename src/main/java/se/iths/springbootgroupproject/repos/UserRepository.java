179e4c3722d5bdd12016bd0563d6963ac92f710d
Emmelie83 <info@emmeliejohansson.com>
2024-03-21 14:43:57 +0100

    Remove spring banner

--- src/main/java/se/iths/springbootgroupproject/repos/UserRepository.java
+++ src/main/java/se/iths/springbootgroupproject/repos/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.repository.ListCrudRepository;
 import se.iths.springbootgroupproject.entities.User;
 import java.util.List;
 public interface UserRepository extends ListCrudRepository<User, Long> {
-    List<User> findByName(String name);
 
 
     @EntityGraph(attributePaths = "allUsers")
