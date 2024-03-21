1182b79e513a07b077db8e7a3e78f67c8d50df15
Emmelie83 <info@emmeliejohansson.com>
2024-03-21 02:32:10 +0100

    Initial commit

---
+++ src/main/java/se/iths/springbootgroupproject/SpringBootGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package se.iths.springbootgroupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGroupProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGroupProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/springbootgroupproject/SpringBootGroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.springbootgroupproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGroupProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
