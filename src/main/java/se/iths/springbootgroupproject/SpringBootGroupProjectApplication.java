2bf55accb86bb6366863fe3085a042a0766d8d8f
Emmelie83 <info@emmeliejohansson.com>
2024-03-25 16:46:08 +0100

    Add Eventlisteners to set date automatically when posting a message

--- src/main/java/se/iths/springbootgroupproject/SpringBootGroupProjectApplication.java
+++ src/main/java/se/iths/springbootgroupproject/SpringBootGroupProjectApplication.java
@@ -2,8 +2,10 @@ package se.iths.springbootgroupproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringBootGroupProjectApplication {
 
     public static void main(String[] args) {
